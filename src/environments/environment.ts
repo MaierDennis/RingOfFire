b6e85aad6b9927334f4f90381f24ac94238dd903
Dennis Maier <maier.dennis@outlook.de>
2023-01-30 12:59:26 +0100

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-f7fa4',
+    appId: '1:272017337728:web:c399cd73bfeb7dd5bba565',
+    storageBucket: 'ring-of-fire-f7fa4.appspot.com',
+    apiKey: 'AIzaSyA81HmiIq2klNOGeuShFNQIEkFUX1FAIkQ',
+    authDomain: 'ring-of-fire-f7fa4.firebaseapp.com',
+    messagingSenderId: '272017337728',
+  },};
